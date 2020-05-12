aa03144b636216e249785e2250c9350a216e31ab
Effanville <>
2020-05-12 22:20:10 +0100

    Add common objects to StructureCommon.

---
+++ StructureCommon/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+// Which testing suite internals are visible to
+[assembly: InternalsVisibleTo("FinancialStructures_UnitTests")]
+[assembly: InternalsVisibleTo("FPD_UI_UnitTests")]
