a1e7323e56b8d1f15fe4e1e81d1d48a560eda0e8
Effanville <matt.egginton@gmail.com>
2020-05-14 22:28:10 +0100

    Add Testing projects and add relevant tests.

--- StructureCommon/Properties/AssemblyInfo.cs
+++ StructureCommon/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@
 // Which testing suite internals are visible to
 [assembly: InternalsVisibleTo("FinancialStructures_UnitTests")]
 [assembly: InternalsVisibleTo("FPD_UI_UnitTests")]
+[assembly: InternalsVisibleTo("StructureCommon.Tests")]
