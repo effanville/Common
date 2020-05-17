76f5871d61a3ad5a190457f0a3550acb935bd1a4
Effanville <>
2020-05-17 10:40:01 +0100

    Move default Name class into common library.

---
+++ UICommon.Tests/Converters/NameToStringConverterTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using StructureCommon.NamingStructures;
+using UICommon.Converters;
+
+namespace UICommon.Tests.Converters
+{
+    [TestFixture]
+    public sealed class PlayerNameToStringTests
+    {
+        [TestCase("Bloggs", "Joe")]
+        [TestCase("Bloggs", "")]
+        [TestCase("", "Joe")]
+        [TestCase("Bloggs", null)]
+        [TestCase(null, "Joe")]
+        [TestCase(null, null)]
+        public void ConvertCorrectly(string surname, string forename)
+        {
+            var name = new Name(surname, forename);
+
+            var converter = new NameToStringConverter();
+
+            var converted = converter.Convert(name, null, null, null);
+            Assert.AreEqual(name.ToString(), converted);
+        }
+
+        [TestCase("Bloggs", "Joe")]
+        [TestCase("Bloggs", "")]
+        [TestCase("", "Joe")]
+        [TestCase("Bloggs", null)]
+        [TestCase(null, "Joe")]
+        [TestCase(null, null)]
+        public void ConvertBackCorrectly(string surname, string forename)
+        {
+            var name = new Name(surname, forename);
+
+            var converter = new NameToStringConverter();
+
+            //This test is broken because it required a specific ToSTring that has changed
+            // when moving into this solution. Need to rewrite test to fix.
+            var converted = converter.ConvertBack(name.ToString(), null, null, null);
+            Assertions.NamesEqual(name, (Name)converted);
+        }
+    }
+}
