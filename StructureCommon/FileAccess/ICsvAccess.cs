1a488e8bd9653dbc106d4f782126625a7ef68442
Effanville <matt.egginton@gmail.com>
2020-05-27 22:10:10 +0100

    Add comments to CSV access interface.

--- StructureCommon/FileAccess/ICsvAccess.cs
+++ StructureCommon/FileAccess/ICsvAccess.cs
@@ -4,10 +4,19 @@ using StructureCommon.Reporting;
 
 namespace StructureCommon.FileAccess
 {
+    /// <summary>
+    /// Contains methods to obtain and export data from csv file
+    /// </summary>
     public interface ICSVAccess
     {
+        /// <summary>
+        /// obtains a list of data from the csv file.
+        /// </summary>
         List<object> CreateDataFromCsv(List<string[]> valuationsToRead, IReportLogger reportLogger = null);
 
+        /// <summary>
+        /// Exports data to a csv file.
+        /// </summary>
         void WriteDataToCsv(TextWriter writer, IReportLogger reportLogger = null);
     }
 }
