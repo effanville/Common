e59ff57f9fe5dbe11dd9e95344eb6b8f294b182a
Effanville <>
2020-05-31 22:29:40 +0100

    Modify publishing profiles so package version is set.

--- StructureCommon/Properties/AssemblyInfo.cs
+++ StructureCommon/Properties/AssemblyInfo.cs
@@ -1,6 +1,10 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 // Which testing suite internals are visible to
 [assembly: InternalsVisibleTo("FinancialStructures_UnitTests")]
 [assembly: InternalsVisibleTo("FPD_UI_UnitTests")]
 [assembly: InternalsVisibleTo("StructureCommon.Tests")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
