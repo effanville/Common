99d346216de205de0cb97dc7257f435ba2a0c9d7
Effanville <>
2020-12-03 23:20:48 +0000

    Add Selectable class that wraps another object with a boolean.

---
+++ UICommon/DisplayClasses/Selectable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace UICommon.DisplayClasses
+{
+    /// <summary>
+    /// Class that wraps a boolean around an instance of a type <typeparamref name="T"/>
+    /// </summary>
+    /// <typeparam name="T">An object that can be equated to another.</typeparam>
+    public class Selectable<T>
+    {
+        private bool fSelected;
+        /// <summary>
+        /// A boolean flag associated to the instance.
+        /// </summary>
+        public bool Selected
+        {
+            get
+            {
+                return fSelected;
+            }
+            set
+            {
+                if (fSelected != value)
+                {
+                    fSelected = value;
+                    OnSelectedChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// The instance to hold.
+        /// </summary>
+        public T Instance
+        {
+            get;
+            set;
+        }
+
+        public Selectable(T instance, bool selected)
+        {
+            Selected = selected;
+            Instance = instance;
+        }
+
+        /// <summary>
+        /// Event handler controlling property changed
+        /// </summary>
+        public event EventHandler SelectedChanged;
+
+        /// <summary>
+        /// invokes the event for property changed.
+        /// </summary>
+        private void OnSelectedChanged()
+        {
+            if (SelectedChanged != null)
+            {
+                SelectedChanged(this, new EventArgs());
+            }
+        }
+    }
+}
