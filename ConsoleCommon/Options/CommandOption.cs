33a05ec10615ea6aa478c9401fa22b1a8c7a376c
Effanville <>
2021-02-22 21:41:52 +0000

    Add console common project.

---
+++ ConsoleCommon/Options/CommandOption.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleCommon.Options
+{
+    /// <summary>
+    /// A container for a user specified command line input.
+    /// </summary>
+    public class CommandOption
+    {
+        /// <summary>
+        /// The name of this option. Used to input the value
+        /// for this option in the command line.
+        /// </summary>
+        public string Name
+        {
+            get;
+        }
+
+        /// <summary>
+        /// A description of this option. Printed in help docs.
+        /// </summary>
+        public string Description
+        {
+            get;
+        }
+
+        /// <summary>
+        /// The value input by the user, before parsing.
+        /// </summary>
+        public string InputValue
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The mechanism to validate the option.
+        /// </summary>
+        public Func<object, bool> Validator
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Any error gone wrong in validation.
+        /// </summary>
+        public string ErrorMessage
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// The value after validation.
+        /// </summary>
+        public virtual object ValueAsObject
+        {
+            get;
+            private set;
+        }
+
+        public CommandOption(string name, string description, Func<object, bool> validator)
+        {
+            Name = name;
+            Description = description;
+            Validator = validator;
+        }
+
+        /// <summary>
+        /// Validate the already specified <see cref="InputValue"/> value based on the validation 
+        /// specified in construction.
+        /// </summary>
+        public virtual bool Validate()
+        {
+            ValueAsObject = InputValue;
+            return Validator(InputValue);
+        }
+    }
+}
