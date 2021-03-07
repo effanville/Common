9d65c07bb9ef8432db6d5a32293799c69e8771f9
Effanville <matt.egginton@gmail.com>
2021-03-07 21:31:10 +0000

    Add new SelectableEquatable to enable selecting Equatable and not equatable objects

--- StructureCommon/DisplayClasses/SelectableHelpers.cs
+++ StructureCommon/DisplayClasses/SelectableHelpers.cs
@@ -9,7 +9,8 @@ namespace StructureCommon.DisplayClasses
         /// Returns the boolean associated to a specific instance <paramref name="nameToSearch"/>
         /// out of the list <paramref name="names"/>. Returns false if it is not found.
         /// </summary>
-        public static bool GetData<T>(List<Selectable<T>> names, T nameToSearch) where T : IEquatable<T>
+        public static bool GetData<T>(List<SelectableEquatable<T>> names, T nameToSearch)
+            where T : IEquatable<T>, IComparable
         {
             foreach (var name in names)
             {
