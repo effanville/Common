fa86258ef7a9eb6b435c3791f043eecf4429f95a
effanville <49975008+effanville@users.noreply.github.com>
2021-05-05 22:22:43 +0100

    Ensure ErrorReports are threadsafe. (#4)
    Place locks on adding or accessing reports.
    Take a copy of reports prior to accessing data.
    Returns a shallow copy of any list of reports.
    Various code cleanup tasks.

---
+++ StructureCommon.Tests/Reporting/ErrorReportsTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using StructureCommon.Reporting;
+
+namespace StructureCommon.Tests.Reporting
+{
+    [TestFixture]
+    public sealed class ErrorReportsTests
+    {
+        [Test]
+        public void AccessAfterClearingTest()
+        {
+            ErrorReports reports = new ErrorReports();
+            string errorString = "some problem";
+            reports.AddError(errorString);
+
+            var shallowCopy = reports.GetReports();
+            Assert.AreEqual(1, shallowCopy.Count);
+
+            reports.Clear();
+
+            Assert.AreEqual(1, shallowCopy.Count);
+            var onlyOne = shallowCopy.Single();
+            Assert.AreEqual(errorString, onlyOne.Message);
+        }
+    }
+}
