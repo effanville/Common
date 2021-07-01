e237b9c91cbf33ea04fe21a41f73f49aad322042
Effanville <>
2021-07-01 17:18:21 +0100

    Add UiGlobals object to UiCommon.

---
+++ Common.UI/DispatcherInstance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Common.UI
+{
+    /// <summary>
+    /// An implementation of <see cref="IDispatcher"/> using
+    /// the <see cref="Application.Current.Dispatcher"/> to
+    /// dispatch.
+    /// </summary>
+    public class DispatcherInstance : IDispatcher
+    {
+        private readonly Dispatcher fDispatcher;
+
+        public DispatcherInstance()
+        {
+            fDispatcher = Application.Current.Dispatcher;
+        }
+
+        /// <inheritdoc/>
+        public void Invoke(Action action)
+        {
+            fDispatcher.Invoke(action);
+        }
+
+        /// <inheritdoc/>
+        public void BeginInvoke(Action action)
+        {
+            _ = fDispatcher.BeginInvoke(action);
+        }
+    }
+}
