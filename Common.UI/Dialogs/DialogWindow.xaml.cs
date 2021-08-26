e929f9a8429266a1035fd40d2b261541093b6443
Effanville <>
2021-08-26 23:48:48 +0100

    Tidy up version generation of projects.

--- Common.UI/Dialogs/DialogWindow.xaml.cs
+++ Common.UI/Dialogs/DialogWindow.xaml.cs
@@ -5,7 +5,7 @@ namespace Common.UI.Dialogs
 {
     /// <summary>
     /// Interaction logic for DialogWindow.xaml
-    /// This is a container window for any dialogs that are opened via 
+    /// This is a container window for any dialogs that are opened via
     /// the Dialog service.
     /// </summary>
     public sealed partial class DialogWindow : Window, ICloseable
