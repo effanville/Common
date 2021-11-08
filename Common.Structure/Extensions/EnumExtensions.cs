7a4f26328fd9730204ed9d45283e1079acd70946
Effanville <>
2021-11-08 22:51:35 +0000

    Add a simple enum extension.

---
+++ Common.Structure/Extensions/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Common.Structure.Extensions
+{
+    /// <summary>
+    /// Miscellaneous custom extension functions for the <see cref="Enum"/> type.
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Returns the longest length of the enum names.
+        /// </summary>
+        /// <typeparam name="T">The Enum type to query.</typeparam>
+        /// <returns>An int of the longest number of characters in the string name for the Enum.</returns>
+        public static int LongestEntry<T>() where T : Enum
+        {
+            string[] values = Enum.GetValues(typeof(T)).Cast<T>().Select(item => item.ToString()).ToArray();
+            return values.Max(item => item.Length);
+        }
+    }
+}
