f87ccd88cc35dbc3b007afcf152c260091f8a628
Effanville <>
2021-11-15 21:48:23 +0000

    Add Decimal Extensions.

---
+++ Common.Structure.Tests/Extensions/DecimalExtensionsTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Common.Structure.Extensions;
+
+using NUnit.Framework;
+
+namespace Common.Structure.Tests.Extensions
+{
+    [TestFixture]
+    public sealed class DecimalExtensionsTests
+    {
+        private static IEnumerable<TestCaseData> TruncateToStringTestsData()
+        {
+            yield return new TestCaseData(0.000000000m, 2, "0");
+            yield return new TestCaseData(0.42345678m, 2, "0.42");
+            yield return new TestCaseData(12345.000000000m, 2, "12345");
+            yield return new TestCaseData(123.12345m, 3, "123.123");
+        }
+
+        [TestCaseSource(nameof(TruncateToStringTestsData))]
+        public void TruncateToStringTests(decimal value, int decimalPlaces, string expected)
+        {
+            string truncated = value.TruncateToString(decimalPlaces);
+            Assert.AreEqual(expected, truncated);
+        }
+        private static IEnumerable<TestCaseData> TruncateTestsData()
+        {
+            yield return new TestCaseData(0.000000000m, 2, 0.00m);
+            yield return new TestCaseData(0.42345678m, 2, 0.42m);
+            yield return new TestCaseData(12345.000000000m, 2, 12345.00m);
+            yield return new TestCaseData(123.12345m, 3, 123.123m);
+        }
+
+        [TestCaseSource(nameof(TruncateTestsData))]
+        public void TruncateTests(decimal value, int decimalPlaces, decimal expected)
+        {
+            decimal truncated = value.Truncate(decimalPlaces);
+            Assert.AreEqual(expected, truncated);
+        }
+
+        private static IEnumerable<TestCaseData> EqualsTestsData()
+        {
+            yield return new TestCaseData(0.0m, 0.0m, 0.0m, true);
+            yield return new TestCaseData(0.0m, 0.1m, 0.0m, false);
+            yield return new TestCaseData(0.0m, 0.0m, 0.0000001m, true);
+            yield return new TestCaseData(0.01m, 0.0101m, 0.001m, true);
+            yield return new TestCaseData(0.01m, 0.0001m, 0.001m, false);
+            yield return new TestCaseData(0.01m, 0.011m, 0.0001m, false);
+            yield return new TestCaseData(10m, 20m, 10m, false);
+            yield return new TestCaseData(10m, 20m, 9m, false);
+            yield return new TestCaseData(10m, 20m, 11m, true);
+        }
+
+        [TestCaseSource(nameof(EqualsTestsData))]
+        public void EqualsTests(decimal first, decimal second, decimal tol, bool expected)
+        {
+            bool actual = first.Equals(second, tol);
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static IEnumerable<TestCaseData> PowTestsData()
+        {
+            yield return new TestCaseData(1.0m, 0, 1.0m);
+            yield return new TestCaseData(1.0m, 1, 1.0m);
+            yield return new TestCaseData(2.0m, 0, 1.0m);
+            yield return new TestCaseData(2.0m, 2, 4.0m);
+            yield return new TestCaseData(2.0m, 10, 1024.0m);
+            yield return new TestCaseData(2.5m, 2, 6.25m);
+        }
+
+        [TestCaseSource(nameof(PowTestsData))]
+        public void PowTests(decimal first, int exp, decimal expected)
+        {
+            decimal actual = first.Pow(exp);
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static IEnumerable<TestCaseData> PowErrorTestsData()
+        {
+            yield return new TestCaseData(1.0m, -1);
+        }
+
+        [TestCaseSource(nameof(PowErrorTestsData))]
+        public void PowErrorTests(decimal first, int exp)
+        {
+            _ = Assert.Throws<ArgumentOutOfRangeException>(() => { _ = first.Pow(exp); });
+        }
+    }
+}
