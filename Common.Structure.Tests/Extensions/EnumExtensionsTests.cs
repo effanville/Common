ebef16b5c302b6e2e09203453f790d093e513624
Effanville <>
2021-11-15 22:13:53 +0000

    Add EnumExtensions tests.

---
+++ Common.Structure.Tests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using Common.Structure.Extensions;
+
+using NUnit.Framework;
+
+namespace Common.Structure.Tests.Extensions
+{
+    [TestFixture]
+    public sealed class EnumExtensionsTests
+    {
+        private enum TestEnum1
+        {
+            Value,
+            ValueToo
+        }
+
+        private enum TestEnum2
+        {
+            Value,
+            Val
+        }
+
+        [Test]
+        public void LongestEntryTests()
+        {
+            int length = EnumExtensions.LongestEntry<TestEnum1>();
+            Assert.AreEqual(8, length);
+
+            int enum2Length = EnumExtensions.LongestEntry<TestEnum2>();
+            Assert.AreEqual(5, enum2Length);
+        }
+    }
+}
