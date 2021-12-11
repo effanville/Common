18f37c72a81043708c721f22a3d13cdabcdddc5b
Effanville <>
2021-12-11 17:11:16 +0000

    Add new BracketMethod and unit tests.

---
+++ Common.Structure/MathLibrary/MathConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common.Structure.MathLibrary
+{
+    public static class MathConstants
+    {
+        public const double Pi = Math.PI;
+        public const double GoldenRatio = 1.61803398875;
+        public const double InverseGoldenRatio = 0.61803399;
+        public const double C = 1 - InverseGoldenRatio;
+        public const double Tiny = 1e-20;
+    }
+}
