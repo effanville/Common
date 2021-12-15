4539b30375caef95b00df40cbf80fe12b5a0a9de
Effanville <>
2021-12-15 23:13:52 +0000

    Add basic class for holding a result value or an error.

---
+++ Common.Structure/MathLibrary/Result.cs
@@ -0,0 +1,15 @@
+﻿namespace Common.Structure.MathLibrary
+{
+    public sealed class Result
+    {
+        /// <summary>
+        /// Create an error result from an error.
+        /// </summary>
+        /// <typeparam name="T">The type of the result to return.</typeparam>
+        /// <param name="error">The error to encapsulate.</param>
+        public static Result<T> ErrorResult<T>(string error)
+        {
+            return new Result<T>(error);
+        }
+    }
+}
---
+++ Common.Structure/MathLibrary/Result{T}.cs
@@ -0,0 +1,57 @@
+﻿namespace Common.Structure.MathLibrary
+{
+    /// <summary>
+    /// A result class containing a value of a result or an Error message.
+    /// </summary>
+    /// <typeparam name="T">The type of the value.</typeparam>
+    public class Result<T>
+    {
+        /// <summary>
+        /// The resultant value to return.
+        /// </summary>
+        public T Value
+        {
+            get;
+        }
+
+        /// <summary>
+        /// The error in case of no value being returned.
+        /// </summary>
+        public string Error
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Create an instance from a value.
+        /// </summary>
+        public Result(T value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Create an instance from an error.
+        /// </summary>
+        public Result(string error)
+        {
+            Error = error;
+        }
+
+        /// <summary>
+        /// Create a Result object implicitly from a value.
+        /// </summary>
+        public static implicit operator Result<T>(T value)
+        {
+            return new Result<T>(value);
+        }
+
+        /// <summary>
+        /// Is the result an error result.
+        /// </summary>
+        public bool IsError()
+        {
+            return !string.IsNullOrEmpty(Error);
+        }
+    }
+}
