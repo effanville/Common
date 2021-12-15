3ae02e85effccf209fa84587265242ebd63290dc
Effanville <>
2021-12-15 23:14:10 +0000

    Implement Bisection root finding method.

---
+++ Common.Structure/MathLibrary/RootFinding/RootBracket.cs
@@ -0,0 +1,116 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Common.Structure.MathLibrary.RootFinding
+{
+    public static class RootBracket
+    {
+        public sealed class Bracket
+        {
+            public double LowerBound
+            {
+                get;
+            }
+
+            public double UpperBound
+            {
+                get;
+            }
+
+            public Bracket(double lowerBound, double upperBound)
+            {
+                LowerBound = lowerBound;
+                UpperBound = upperBound;
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is Bracket result &&
+                       LowerBound == result.LowerBound &&
+                       UpperBound == result.UpperBound;
+            }
+
+            public override int GetHashCode()
+            {
+                int hashCode = 17;
+                hashCode = 23 * hashCode + LowerBound.GetHashCode();
+                hashCode = 23 * hashCode + UpperBound.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        /// <summary>
+        /// Finds a pair of lower bound and upper bound for a root of a function
+        /// within the given number of iterations.<br/>
+        /// This expands geometrically outwards until a bracket is found.
+        /// </summary>
+        public static Result<Bracket> FindBracketForRoot(
+            double lowerGuess,
+            double upperGuess,
+            Func<double, double> func,
+            int maxIterations = 50)
+        {
+            if (lowerGuess == upperGuess)
+            {
+                // bad initial range guess
+                return null;
+            }
+
+            double f1 = func(lowerGuess);
+            double f2 = func(upperGuess);
+
+            for (int j = 0; j < maxIterations; j++)
+            {
+                if (f1 * f2 < 0)
+                {
+                    return new Bracket(lowerGuess, upperGuess);
+                }
+
+                if (Math.Abs(f1) < Math.Abs(f2))
+                {
+                    f1 = func(lowerGuess += MathConstants.GoldenRatio * (lowerGuess - upperGuess));
+                }
+                else
+                {
+                    f2 = func(upperGuess += MathConstants.GoldenRatio * (upperGuess - lowerGuess));
+                }
+            }
+
+            return Result.ErrorResult<Bracket>("Exceeded max number of iterations.");
+        }
+
+        /// <summary>
+        /// Given a function <paramref name="func"/> and bounds <paramref name="lowerBound"/> and <paramref name="upperBound"/> for a root, subdivide this region into <paramref name="numberSubDivisions"/>
+        /// and return all roots found in these subdivisions. Stop once reached <paramref name="maxDesiredNumberRoots"/>.
+        /// </summary>
+        public static List<Bracket> NarrowBracketForRoot(
+            double lowerBound,
+            double upperBound,
+            Func<double, double> func,
+            int numberSubDivisions,
+            int maxDesiredNumberRoots)
+        {
+            double dx = (upperBound - lowerBound) / numberSubDivisions;
+            double x = lowerBound;
+            double fp = func(x);
+            var brackets = new List<Bracket>();
+            for (int i = 0; i < numberSubDivisions; i++)
+            {
+                double fc = func(x += dx);
+                if (fc * fp <= 0)
+                {
+                    brackets.Add(new Bracket(x - dx, x));
+                    if (brackets.Count > maxDesiredNumberRoots)
+                    {
+                        return brackets;
+                    }
+                }
+
+                fp = fc;
+            }
+
+            return brackets;
+        }
+    }
+}
