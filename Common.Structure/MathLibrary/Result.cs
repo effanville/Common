251371af8bb77a9e5469a76a9359014f988e1fe4
Effanville <>
2021-12-15 23:13:52 +0000

    Add basic class for holding a result value or an error.

---
+++ Common.Structure/MathLibrary/Result.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Structure.MathLibrary
+{
+    public sealed class Result
+    {
+        public static Result<T> ErrorResult<T>(string error)
+        {
+            return new Result<T>(error);
+        }
+    }
+}
---
+++ Common.Structure/MathLibrary/Result{T}.cs
@@ -0,0 +1,35 @@
+﻿namespace Common.Structure.MathLibrary
+{
+    public sealed class Result<T>
+    {
+        public T Value
+        {
+            get;
+        }
+
+        public string Error
+        {
+            get;
+        }
+
+        public Result(T value)
+        {
+            Value = value;
+        }
+
+        public Result(string error)
+        {
+            Error = error;
+        }
+
+        public static implicit operator Result<T>(T value)
+        {
+            return new Result<T>(value);
+        }
+
+        public bool IsError()
+        {
+            return !string.IsNullOrEmpty(Error);
+        }
+    }
+}
