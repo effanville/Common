a9d77cea31ba940a1c798ea9284d2b5fb659295e
Effanville <>
2021-12-15 23:14:10 +0000

    Implement Bisection root finding method.

---
+++ Common.Structure/MathLibrary/RootFinding/RootFinder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Common.Structure.MathLibrary.RootFinding
+{
+    public static partial class RootFinder
+    {
+        public enum Type
+        {
+            Bisection
+        }
+
+        public static Result<double> FindRoot(
+            Type rootFinderType,
+            Func<double, double> func,
+            double lowerBound,
+            double upperBound,
+            int maxIterations = 100,
+            double tolerance = 1e-8)
+        {
+            switch (rootFinderType)
+            {
+                case Type.Bisection:
+                {
+                    return Bisection.FindRoot(func, lowerBound, upperBound, maxIterations, tolerance);
+                }
+                default:
+                    return null;
+            }
+        }
+    }
+}
