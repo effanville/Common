829dc8706ddfa69c7e228bd3545065113a0a0705
Effanville <>
2021-12-15 23:33:03 +0000

    Add Generic OptimisationResult classes.

--- Common.Structure/MathLibrary/Optimisation/Helpers.cs
+++ Common.Structure/MathLibrary/Optimisation/Helpers.cs
@@ -1,4 +1,6 @@
-﻿namespace Common.Structure.MathLibrary.Optimisation
+﻿using System;
+
+namespace Common.Structure.MathLibrary.Optimisation
 {
     public static class Helpers
     {
@@ -13,6 +15,11 @@
             first = second;
             second = third;
             third = d;
+        }
+
+        public static double Sign(double a, double b)
+        {
+            return ((b) >= 0.0 ? Math.Abs(a) : -Math.Abs(a));
         }
     }
 }
