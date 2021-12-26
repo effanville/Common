35942d3e07676669e0962df2af6931d1cc04296c
Effanville <>
2021-12-26 12:47:42 +0000

    Tidy LUDecomposition code.

--- Common.Structure.Tests/MathLibrary/Matrices/LUDecompositionTests.cs
+++ Common.Structure.Tests/MathLibrary/Matrices/LUDecompositionTests.cs
@@ -11,12 +11,13 @@ namespace Common.Structure.Tests.MathLibrary.Matrices
         public void LUDecompCorrect([Values(1, 2, 3, 4, 5, 6, 7, 8)] int expectedMatrixIndex)
         {
             TestMatrixValues matrix = MatrixTestHelper.GetMatrix(expectedMatrixIndex);
-            LUDecomposition lUDecomposition = new LUDecomposition(matrix.Matrix);
-            double[,] product = lUDecomposition.LowerDecomp.Multiply(lUDecomposition.UpperDecomp);
+            LUDecomposition lUDecomposition = LUDecomposition.Generate(matrix.Matrix).Value;
+            double[,] product = lUDecomposition.Lower.Multiply(lUDecomposition.Upper);
+
             Assertions.AreEqual(matrix.Matrix, product, 1e-3, "products wrong");
-            Assert.AreEqual(lUDecomposition.Invertible, true);
-            Assertions.AreEqual(matrix.Upper, lUDecomposition.UpperDecomp, 1e-3, "Upper wrong");
-            Assertions.AreEqual(matrix.Lower, lUDecomposition.LowerDecomp, 1e-3, "Lower wrong");
+
+            Assertions.AreEqual(matrix.Upper, lUDecomposition.Upper, 1e-3, "Upper wrong");
+            Assertions.AreEqual(matrix.Lower, lUDecomposition.Lower, 1e-3, "Lower wrong");
         }
     }
 }
