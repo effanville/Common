7db4e9ec89328675b0460e5d0af2a9fa682a062d
Effanville <>
2021-12-26 13:19:49 +0000

    Add FalsePosition, Ridders and Secant methods to root finding.

---
+++ Common.Structure/MathLibrary/RootFinding/FalsePositionMethod.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Common.Structure.MathLibrary.RootFinding
+{
+    /// <summary>
+    /// Routine to calculate a 1D root via the FalsePosition method.
+    /// </summary>
+    public static class FalsePositionMethod
+    {
+        public static Result<double> Root(
+           Func<double, double> func,
+           double lowerBound,
+           double upperBound,
+           int maxIterations = 30,
+           double tolerance = 1e-8)
+        {
+            double fLower = func(lowerBound);
+            double fUpper = func(upperBound);
+            double xLower;
+            double xUpper;
+            double swap;
+            if (fLower * fUpper > 0.0)
+            {
+                return Result.ErrorResult<double>("Root must be bracketed for False Position to work.");
+            }
+
+            if (fLower < 0.0)
+            {
+                xLower = lowerBound;
+                xUpper = upperBound;
+            }
+            else
+            {
+                xLower = upperBound;
+                xUpper = lowerBound;
+                swap = fLower;
+                fLower = fUpper;
+                fUpper = swap;
+            }
+
+            double dx = xUpper - xLower;
+            double rootCandidate;
+            double fRootCandidate;
+            double del;
+            for (int j = 0; j < maxIterations; j++)
+            {
+                rootCandidate = xLower + dx + fLower / (fLower - fUpper);
+                fRootCandidate = func(rootCandidate);
+                if (fRootCandidate < 0.0)
+                {
+                    del = xLower - rootCandidate;
+                    xLower = rootCandidate;
+                    fLower = fRootCandidate;
+                }
+                else
+                {
+                    del = xUpper - rootCandidate;
+                    xUpper = rootCandidate;
+                    fUpper = fRootCandidate;
+                }
+
+                dx = xUpper - xLower;
+                if (Math.Abs(del) < tolerance || fRootCandidate == 0.0)
+                {
+                    return rootCandidate;
+                }
+            }
+
+            return Result.ErrorResult<double>("Exceeded max number of iterations.");
+        }
+    }
+}
---
+++ Common.Structure/MathLibrary/RootFinding/SecantMethod.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Common.Structure.MathLibrary.RootFinding
+{
+    /// <summary>
+    /// Routine to calculate a 1D root via the Secant method.
+    /// </summary>
+    public static class SecantMethod
+    {
+        public static Result<double> Root(
+           Func<double, double> func,
+           double lowerBound,
+           double upperBound,
+           int maxIterations = 30,
+           double tolerance = 1e-8)
+        {
+            double fLower = func(lowerBound);
+            double f = func(upperBound);
+            double xLower;
+            double swap;
+            double rootCandidate;
+            if (Math.Abs(fLower) < Math.Abs(f))
+            {
+                rootCandidate = lowerBound;
+                xLower = upperBound;
+                swap = fLower;
+                fLower = f;
+                f = swap;
+            }
+            else
+            {
+                xLower = lowerBound;
+                rootCandidate = upperBound;
+            }
+            double dx;
+            for (int j = 0; j < maxIterations; j++)
+            {
+                dx = (xLower - rootCandidate) * f / (f - fLower);
+                xLower = rootCandidate;
+                fLower = f;
+                rootCandidate += dx;
+                f = func(rootCandidate);
+                if (Math.Abs(dx) < tolerance || f == 0.0)
+                {
+                    return rootCandidate;
+                }
+            }
+
+            return Result.ErrorResult<double>("Exceeded max number of iterations.");
+        }
+    }
+}
