9e1aa4a3bf3d98005cc2692cebfdcf3bbf55e0b6
Effanville <>
2021-12-31 22:05:38 +0000

    Add multi dimensional Newton Raphson method.

--- Common.Structure.Tests/MathLibrary/RootFinding/NewtonRaphsonTests.cs
+++ Common.Structure.Tests/MathLibrary/RootFinding/NewtonRaphsonTests.cs
@@ -24,5 +24,25 @@ namespace Common.Structure.Tests.MathLibrary.RootFinding
             Assert.IsFalse(bisectionResult.IsError());
             Assert.That(Math.Abs(bisectionResult.Value - 2), Is.LessThan(1e-8));
         }
+
+
+        [Test]
+        public void FindRootMulti()
+        {
+            double[] func(double[] x) => new double[] { (x[0] - 2) * (x[0] - 4), (x[1] - 3) * (x[1] - 5) };
+            double[,] derivative(double[] input)
+            {
+                double[,] result = new double[input.Length, input.Length];
+                result[0, 0] = 2 * input[0] - 6;
+                result[1, 1] = 2 * input[1] - 8;
+
+                return result;
+            }
+            var bisectionResult = NewtonRaphsonMethod.FindMultiDimRoot(func, x => derivative(x), new double[] { 1, 2 });
+            Assert.IsFalse(bisectionResult.IsError());
+            double[] value = bisectionResult.Value;
+            Assert.That(Math.Abs(value[0] - 2), Is.LessThan(1e-8));
+            Assert.That(Math.Abs(value[1] - 3), Is.LessThan(1e-8));
+        }
     }
 }
