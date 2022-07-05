e6dc745797314fa0ff33986e38d4957419f80de4
Effanville <>
2022-07-05 23:16:59 +0100

    Add StatCollection classes.

---
+++ Common.Structure/Statistics/StatCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.IO.Abstractions;
+using System.Text;
+
+using Common.Structure.ReportWriting;
+
+namespace Common.Statistics
+{
+    public static class StatCollectionExtensions
+    {
+        /// <summary>
+        /// Export the collection to a file.
+        /// </summary>
+        /// <param name="collection">The collection to export.</param>
+        /// <param name="fileSystem"></param>
+        /// <param name="filePath"></param>
+        /// <param name="exportType"></param>
+        public static void ExportStats<S, T>(this IGenericStatCollection<S, T> collection, IFileSystem fileSystem, string filePath, DocumentType exportType)
+            where S : Enum
+            where T : class
+        {
+            try
+            {
+                StringBuilder sb = collection.ExportStats(exportType, DocumentElement.h1);
+
+                using (Stream stream = fileSystem.FileStream.Create(filePath, FileMode.Create))
+                using (StreamWriter fileWriter = new StreamWriter(stream))
+                {
+                    fileWriter.WriteLine(sb.ToString());
+                }
+            }
+            catch (IOException)
+            {
+                return;
+            }
+        }
+    }
+}
