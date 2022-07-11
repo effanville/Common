8dfcc43b271a86b6d8b49816a1b078d1771bde81
Effanville <>
2022-07-11 21:55:09 +0100

    Add record classes.

---
+++ Common.Structure/NamingStructures/NameDurationRecord{T}.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Common.Structure.NamingStructures
+{
+    public sealed class NameDurationRecord<T>
+    {
+        private readonly string fRecordName;
+        private readonly Func<T, T, T> fAggregation;
+        public Name Name
+        {
+            get;
+            private set;
+        }
+
+        public DateTime Start
+        {
+            get;
+            private set;
+        }
+
+        public DateTime End
+        {
+            get;
+            private set;
+        }
+
+        public T Value
+        {
+            get;
+            private set;
+        }
+
+        public NameDurationRecord(string recordName, Name name, DateTime start, T firstValue, Func<T, T, T> aggregation)
+        {
+            fRecordName = recordName;
+            fAggregation = aggregation;
+            Name = name;
+            Start = start;
+            End = start;
+            Value = firstValue;
+        }
+
+        public void UpdateValue(DateTime newDate, T additionalValue)
+        {
+            Value = fAggregation(Value, additionalValue);
+            End = End < newDate ? newDate : End;
+            Start = Start > newDate ? newDate : Start;
+        }
+
+        public override string ToString()
+        {
+            return $"{fRecordName}-{Name}-{Start}-{End}-{Value}";
+        }
+    }
+}
---
+++ Common.Structure/NamingStructures/NamedRecord{T,S}.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Common.Structure.NamingStructures
+{
+    public sealed class NamedRecord<T, S>
+    {
+        private readonly Func<T, T, T> fTAggregation;
+        private readonly Func<S, S, S> fSAggregation;
+        private readonly string fRecordName;
+        public Name Name
+        {
+            get;
+            private set;
+        }
+
+        public T Value
+        {
+            get;
+            private set;
+        }
+
+        public S SecondValue
+        {
+            get;
+            private set;
+        }
+
+        public NamedRecord(
+            string recordName,
+            Name name,
+            T value,
+            S secondValue,
+            Func<T, T, T> TAggregation,
+            Func<S, S, S> SAggregation)
+        {
+            fRecordName = recordName;
+            fTAggregation = TAggregation;
+            fSAggregation = SAggregation;
+            Name = name;
+            Value = value;
+            SecondValue = secondValue;
+        }
+        public NamedRecord(
+            string recordName,
+            Name name,
+            T value,
+            S secondValue)
+        {
+            fRecordName = recordName;
+            Name = name;
+            Value = value;
+            SecondValue = secondValue;
+        }
+
+        public void UpdateValue(T additionalTValue, S additionalSValue)
+        {
+            if (fTAggregation != null)
+            {
+                Value = fTAggregation(Value, additionalTValue);
+            }
+            if (fSAggregation != null)
+            {
+                SecondValue = fSAggregation(SecondValue, additionalSValue);
+            }
+        }
+
+        public string RecordName()
+        {
+            return fRecordName;
+        }
+        public override string ToString()
+        {
+            return $"{fRecordName}-{Name}-{Value}-{SecondValue}";
+        }
+    }
+}
