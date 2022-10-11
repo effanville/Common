1b8e74273a2e4e7668d76162a09ee9b109789ceb
Effanville <>
2022-10-11 22:48:04 +0100

    Remove tests for missing method.

--- Common.Structure.Tests/MathLibrary/Optimisation/GoldenSectionMinimiserTests.cs
+++ Common.Structure.Tests/MathLibrary/Optimisation/GoldenSectionMinimiserTests.cs
@@ -81,18 +81,6 @@ namespace Common.Structure.Tests.MathLibrary.Optimisation
         }
 
         [TestCaseSource(nameof(MinimumTestData))]
-        public void CalculateMinimumMathNet(TestData data)
-        {
-            var min = GoldenSectionSearch.MinimumFromMathNet(data.Lower, data.Upper, data.Func, 1e-8, 100);
-            Assert.Multiple(() =>
-            {
-                Assert.AreEqual(data.ExpectedResult.ResultValue.Point, min.ResultValue.Point, 1e-8);
-                Assert.AreEqual(data.ExpectedResult.ResultValue.Value, min.ResultValue.Value, 1e-8);
-                Assert.That(min.ReasonForExit, Is.EqualTo(data.ExpectedResult.ReasonForExit));
-            });
-        }
-
-        [TestCaseSource(nameof(MinimumTestData))]
         public void CalculateMinimum(TestData data)
         {
             var min = GoldenSectionSearch.Minimise(data.Lower, data.Upper, data.Func, 1e-8, 100);
