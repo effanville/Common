f60e8677aa36a67eb87ba36d6afed5328213c845
Effanville <>
2023-05-15 21:38:53 +0800

    Add Action<T> signature to TaskQueue.

--- Common.Structure/DataStructures/TaskQueue.cs
+++ Common.Structure/DataStructures/TaskQueue.cs
@@ -23,6 +23,24 @@ public sealed class TaskQueue
     }
 
     /// <summary>
+    /// Add an action to the queue.
+    /// </summary>
+    public void Enqueue<T>(Action<T> action, T obj)
+    {
+        lock (lockRoot)
+        {
+            fPreviousTask = fPreviousTask?.ContinueWith(tsk => action(obj)) ?? Task.Factory.StartNew(Convert(action), obj);
+        }
+    }
+
+    private static Action<object> Convert<T>(Action<T> myActionT)
+    {
+        return myActionT == null
+            ? null
+            : new Action<object>(o => myActionT((T)o));
+    }
+
+    /// <summary>
     /// Add a task to the queue
     /// </summary>
     public void Enqueue(Task currentTask)
