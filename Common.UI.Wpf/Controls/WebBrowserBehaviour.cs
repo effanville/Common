a73abc292962e2ff1a06877d2c8f6989ac60bcd3
Effanville <>
2023-07-06 20:26:07 +0800

    Convert bindable source to target a Webview2 object.

--- Common.UI.Wpf/Controls/WebBrowserBehaviour.cs
+++ Common.UI.Wpf/Controls/WebBrowserBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Controls;
+
+using Microsoft.Web.WebView2.Wpf;
 
 namespace Common.UI.Wpf.Controls
 {
@@ -13,44 +14,46 @@ namespace Common.UI.Wpf.Controls
         /// The bindable source property.
         /// </summary>
         public static readonly DependencyProperty BindableSourceProperty =
-            DependencyProperty.RegisterAttached("BindableSource", typeof(object), typeof(WebBrowserBehaviours), new UIPropertyMetadata(null, BindableSourcePropertyChanged));
+            DependencyProperty.RegisterAttached(
+                "BindableSource",
+                typeof(object),
+                typeof(WebBrowserBehaviours),
+                new UIPropertyMetadata(null, BindableSourcePropertyChanged));
 
         /// <summary>
         /// Returns the bound source. 
         /// </summary>
-        public static object GetBindableSource(DependencyObject obj)
-        {
-            return (string)obj.GetValue(BindableSourceProperty);
-        }
+        public static object GetBindableSource(DependencyObject obj) => (string)obj.GetValue(BindableSourceProperty);
 
         /// <summary>
         /// Sets the source to the value specified.
         /// </summary>
-        public static void SetBindableSource(DependencyObject obj, object value)
-        {
-            obj.SetValue(BindableSourceProperty, value);
-        }
+        public static void SetBindableSource(DependencyObject obj, object value) => obj.SetValue(BindableSourceProperty, value);
 
         /// <summary>
         /// The property for binding the web browser source with.
         /// </summary>
         public static void BindableSourcePropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
         {
-            if (o is not WebBrowser browser)
+            if (o is not WebView2 browser)
             {
                 return;
             }
 
             Uri uri = null;
-
             if (e.NewValue is string)
             {
                 string uriString = e.NewValue as string;
-                uri = string.IsNullOrWhiteSpace(uriString) ? null : new Uri(uriString);
+                uri = string.IsNullOrWhiteSpace(uriString) ? new Uri("http://localhost") : new Uri(uriString);
             }
             else if (e.NewValue is Uri)
             {
                 uri = e.NewValue as Uri;
+            }
+
+            if (uri == null)
+            {
+                uri = new Uri("http://localhost");
             }
 
             browser.Source = uri;
