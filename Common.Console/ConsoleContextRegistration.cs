78ace78acee3dc9c90143cf2ef4d7a8533815b32
Effanville <effanville@users.noreply.github.com>
2024-01-29 22:07:46 +0800

    feat: Add ability to setup a console host for Console applications.
    This has a single entry point `AddConsoleContext` that requires
    - the console arguments
    - the command types
    and then builds and runs the ConsoleContext as desired.

---
+++ Common.Console/ConsoleContextRegistration.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.Console;
+
+/// <summary>
+/// Contains extension methods to register the necessary services for the <see cref="ConsoleContext"/> to run.
+/// </summary>
+public static class ConsoleContextRegistration
+{
+    /// <summary>
+    /// Register the <see cref="ConsoleContext"/> in the container,
+    /// as well as all relevant types to use.
+    /// </summary>
+    public static IServiceCollection AddConsoleContext(
+        this IServiceCollection serviceCollection,
+        IEnumerable<Type> consoleCommandTypes,
+        string[] args)
+    {
+        serviceCollection.AddSingleton<IFileSystem, FileSystem>();
+        serviceCollection.AddReporting();
+        serviceCollection.AddConsoleCommands(consoleCommandTypes);
+        serviceCollection.AddScoped(_ => new ConsoleCommandArgs(args));
+        serviceCollection.AddHostedService<ConsoleHost>();
+        return serviceCollection;
+    }
+}
---
+++ Common.Console/ConsoleInstanceRegistration.cs
@@ -0,0 +1,48 @@
+using System;
+
+using Common.Structure.Reporting;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.Console;
+
+/// <summary>
+/// Registration extensions for console logging and an <see cref="IConsole"/> instance.
+/// </summary>
+public static class ConsoleInstanceRegistration
+{
+    /// <summary>
+    /// Add console logging and report logging to the <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static IServiceCollection AddReporting(
+        this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<IReportLogger, LogReporter>(_ =>
+            new LogReporter(ReportAction, saveInternally: true));
+        return serviceCollection.AddSingleton<IConsole, ConsoleInstance>(
+            _ => new ConsoleInstance(WriteError, WriteLine));
+    }
+
+    private static void WriteError(string text)
+    {
+        var color = System.Console.ForegroundColor;
+        System.Console.ForegroundColor = ConsoleColor.Red;
+        System.Console.WriteLine(text);
+        System.Console.ForegroundColor = color;
+    }
+
+    private static void WriteLine(string text) => System.Console.WriteLine(text);
+
+    private static void ReportAction(ReportSeverity severity, ReportType reportType, string location, string text)
+    {
+        string message = $"[{DateTime.Now}]-({reportType}) - [{location}] - {text}";
+        if (reportType == ReportType.Error)
+        {
+            WriteError(message);
+        }
+        else
+        {
+            WriteLine(message);
+        }
+    }
+}
